e9bd7b59f343e1747a9d83f57447d078e51cf396
priyankaixeet <priyanka.gupta@ixeet.com>
2015-10-10 14:14:13 +0530

    commit

--- app/src/main/java/com/bluecepheid/admin/vdonor/RegisterActivity.java
+++ app/src/main/java/com/bluecepheid/admin/vdonor/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.bluecepheid.admin.vdonor;
 
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
@@ -28,6 +29,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.os.Bundle;
@@ -67,8 +69,11 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
@@ -92,9 +97,9 @@ public class RegisterActivity extends Activity implements LocationListener {
     String p,lp;
     ProgressDialog pDialog;
     SqliteHelper3 sqliteHelper3;
-    private int year, month, day;
+    private int day;
     JSONParser jsonParser = new JSONParser();
-    private Calendar calendar;
+
     CheckBox c1;
     SqliteHelper2 sqliteHelper2;
     String semail,sname;
@@ -108,7 +113,10 @@ public class RegisterActivity extends Activity implements LocationListener {
     List<Address> addresses;
     LocationManager locationManager;
     TextView latt,longg;
-
+    private Calendar calendar;
+    private int year, month, datee;
+    DatePickerDialog dpd;
+    SimpleDateFormat sdf;
     int[] districIdArr=null;
     SqliteHelper1 sqliteHelper1;
     SqliteHelper4 sqliteHelper4;
@@ -143,6 +151,10 @@ public class RegisterActivity extends Activity implements LocationListener {
         sqliteHelper5 = new SqliteHelper5(this);
         latt=(TextView)findViewById(R.id.latt);
         longg=(TextView)findViewById(R.id.longg);
+        calendar=Calendar.getInstance();
+        datee=calendar.get(Calendar.DAY_OF_MONTH);
+        month=calendar.get(Calendar.MONTH);
+        year=calendar.get(Calendar.YEAR);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         if ( !locationManager.isProviderEnabled( LocationManager.NETWORK_PROVIDER ) ) {
 
@@ -344,9 +356,40 @@ if(sname.contains(" "))
             }
         });
         eDob.setOnClickListener(new View.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
             @Override
             public void onClick(View v) {
-                setDate(v);
+               // setDate(v);
+                dpd = new DatePickerDialog(v.getContext(),
+                        new PickDate(), year, month, datee);
+
+                long maxDate = 0;
+                try {
+                    String maxString="31/12/1997";
+                    sdf=new SimpleDateFormat("dd/MM/yyyy");
+                    Date date1=sdf.parse(maxString);
+                    maxDate=date1.getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                dpd.getDatePicker().setMaxDate(maxDate);
+
+
+                long startDate = 0;
+                try {
+                    String dateString = "01/01/1970";
+                    sdf = new SimpleDateFormat("dd/MM/yyyy");
+                    Date dat = sdf.parse(dateString);
+
+                    startDate = dat.getTime();
+
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                dpd.getDatePicker().setMinDate(startDate);
+                dpd.updateDate(year, month, datee);
+                dpd.show();
             }
         });
         spGen.setOnClickListener(new View.OnClickListener() {
@@ -1634,7 +1677,65 @@ if(sname.contains(" "))
         Log.e("Network Testing", "***Not Available***");
         return false;
     }
+    private class PickDate implements DatePickerDialog.OnDateSetListener {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            view.updateDate(year, monthOfYear, dayOfMonth);
 
+            String s=null;
+            if(monthOfYear==0)
+            {
+                s="Jan";
+            }
+            if(monthOfYear==1)
+            {
+                s="Feb";
+            }
+            if(monthOfYear==2)
+            {
+                s="Mar";
+            }
+            if(monthOfYear==3)
+            {
+                s="Apr";
+            }
+            if(monthOfYear==4)
+            {
+                s="May";
+            }
+            if(monthOfYear==5)
+            {
+                s="Jun";
+            }
+            if(monthOfYear==6)
+            {
+                s="Jul";
+            }
+            if(monthOfYear==7)
+            {
+                s="Aug";
+            }
+            if(monthOfYear==8)
+            {
+                s="Sept";
+            }
+            if(monthOfYear==9)
+            {
+                s="Oct";
+            }
+            if(monthOfYear==10)
+            {
+                s="Nov";
+            }
+            if(monthOfYear==11)
+            {
+                s="Dec";
+            }
+            eDob.setText(dayOfMonth+ "-" +s+ "-" + year);
+            date=year+"-"+monthOfYear+"-"+dayOfMonth;
+            dpd.hide();
+        }
+    }
 
     public void showSettingsAlert(String provider) {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(
